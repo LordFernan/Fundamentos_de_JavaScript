066015f1ff50fe334ef16ca745f07ddab45bd4a9
Fernando Aguilar <feraguilar.cs@gmail.com>
2025-10-11 01:47:55 -0500

    Mi primer commit

---
+++ Variables.js
@@ -0,0 +1,38 @@
+
+console.clear()
+console.log("------------------ variables en javascript ------------------ ")
+
+// Code 
+let Name = "Fernando Aguilar"
+console.log(Name)
+
+// Doble para agregar comenatarios 
+
+// Lo no permitido: Poner nombres no descriptivos a las variables 
+let c = "Elver"
+let d = "Elizabeth"
+let e = "Esmeralda" 
+
+// Lo permitido: Poner nombres descriptivos a las variables. No importa si los nombres son largos
+let hermanoMayor = "Elver"
+let HermanaMenor = "Elizabeh"
+let HermanaMenor2 = "Esmeralda"
+
+// Otros ejemplos de nombres permitidos
+let urlDelUsuario = "http://google.com"
+let idDelUsuario = "202220"
+
+// variables 
+let contador=0 // inicializar la variable
+contador = contador + 1
+
+// constantes 
+const PI = 3.1415
+
+// Imprimiendo el valor de la variable contador
+console.log(contador)
+contador ++
+console.log(contador)
+contador++
+console.log(contador)
+console.log(PI)
