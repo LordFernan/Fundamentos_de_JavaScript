c2426d4dd4ec5b156ba8ffe976adbf28cc4ccb53
Fernando Aguilar <feraguilar.cs@gmail.com>
2025-10-11 16:41:55 -0500

    Clase 05: Tipos de Datos en Javascript: Primitivos y complejos

---
+++ 3.TiposdeDatos.js
@@ -0,0 +1,26 @@
+console.clear()
+console.log(" ---------------- Tipos de Datos en JavaScript ------------------ ")
+
+// Primitivos
+let nombre = "Fernando Aguilar" // string 
+let numero = 23 // number
+let esMayorDeEdad = true // boolean
+let noHayValor = null //null
+let noDefinido = undefined // undefined
+let simboloUnico = Symbol("único") //symbol
+let numeroGrande = 2n   //bigint 
+
+
+// Complejos 
+// object
+let carro = {
+    marca: "Tesla",
+    modelo: "Model S"
+}
+
+// Arrays
+let frutas = ["Manzana", "Pera", "Uva"]
+let numeros = [1, 2, 3, 4, 5]
+
+// Functions 
+function saludar(){}
