9a18b51d4b0c2955b96dd5b7edd24bf10cc3687d
Fernando Aguilar <feraguilar.cs@gmail.com>
2025-10-11 19:08:46 -0500

    Clase 10: Operadores de comparacion en JavaScript: Igualdad y Desiguladad

---
+++ 02. Estructuras de control y logica/7.OperadoresComparacion.js
@@ -0,0 +1,25 @@
+console.clear()
+console.log("------------------ Operadores de Comparación ------------------\n")
+
+// Code 
+/*
+ == : comparar dos valores 
+ === : comparar valores y tipo de dato (siempre es el mas recomendavble para evitar errores)
+ != : diferente a nivel de valores 
+ !== : diferente a nivel de valores y tipo de dato 
+ > : mayor 
+ < : menor
+ >= : mayor o igual
+ <= : menor o igual
+
+*/
+
+// Ejemplos 
+const a = 10
+const b = 20
+const c = "10"
+
+console.log(a == b)
+console.log(a == c)
+console.log(a === c)
+console.log(a !== c)
