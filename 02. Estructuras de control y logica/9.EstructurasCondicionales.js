fad290fe18002cf21a63ec04b8c32aa9ee6a29e1
Fernando Aguilar <feraguilar.cs@gmail.com>
2025-10-11 19:46:06 -0500

    clase 12: Estructuras consicionales: Uso de if, else y else if en javascript

---
+++ 02. Estructuras de control y logica/9.EstructurasCondicionales.js
@@ -0,0 +1,16 @@
+console.clear()
+console.log(" --------------------- Estructuras Condicionales ---------------------\n")
+
+// Code 
+// Estructuras de control
+
+// Ejemplo super basico: uso de if, else if y else 
+let nombre = "Juan"
+if(nombre === "Fernando"){
+    console.log("Bienvenido", nombre)
+} else if(nombre === "Elver"){
+    console.log("Bienvenido", nombre)
+}
+else{
+    console.log("Nombre desconocido. No tienes acceso")
+}
