404dc2379afadd50c593392175423f02f9a10628
Fernando Aguilar <feraguilar.cs@gmail.com>
2025-10-11 19:23:44 -0500

    Clase 11: Operadores logico en programacion: and, or y not

---
+++ 02. Estructuras de control y logica/8.OperadoresLogicos.js
@@ -0,0 +1,15 @@
+console.clear()
+console.log("--------------------- Operadores Logicos en JavaScript ------------------------------\n")
+
+// Code 
+// Operadores basicos: and, or y not 
+
+const a = 10
+const b = 20
+const c = "10"
+
+const p1 = !(a===c)
+
+console.log(a==b && a===c)
+console.log(a!=b || a===c)
+console.log(p1)
