2c034ea72b685b5c41b53f2eab0c42908eb8d89f
Fernando Aguilar <feraguilar.cs@gmail.com>
2025-10-15 20:46:02 -0500

    Clase 15: Ciclo For. Iteración de listas y uso de console.log en JavaScript

---
+++ 02. Estructuras de control y logica/12.cicloFor.js
@@ -0,0 +1,14 @@
+console.clear()
+console.log("------------------ Ciclo For ------------------")
+
+
+// Iterar una lista haciendo uso del ciclo for
+let lista = ["comer", "dormir", "Estudiar y ser un pro en CS", "Repetir"]
+for(let i=0; i<lista.length; i++){
+    console.log(lista[i])
+}
+
+
+// Nota: pedirle a la IA que te deje un reto hard para consolidar conocimientos.
+// Además de retos, podrias pedirle que te propronga miniproyectos por cada clase. 
+
